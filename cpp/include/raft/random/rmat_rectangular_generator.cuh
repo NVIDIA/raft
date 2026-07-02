7cb31b4e16f3171b97604ca6a89f3615eff4ead1
Divye Gala <divyegala@gmail.com>
2026-07-02 20:13:06 +0000

    style check

--- cpp/include/raft/random/rmat_rectangular_generator.cuh
+++ cpp/include/raft/random/rmat_rectangular_generator.cuh
@@ -1,6 +1,6 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION.
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * reserved. reserved. reserved. reserved. SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
