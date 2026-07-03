700e4c9640fa8d880acdca8555476330d158f370
achirkin <achirkin@users.noreply.github.com>
2026-07-03 15:11:00 +0200

    revert/bring back res.has_resource_factory check in resource getters to avoid overheads of creating and discarding the factories

--- cpp/include/raft/core/resource/cublaslt_handle.hpp
+++ cpp/include/raft/core/resource/cublaslt_handle.hpp
@@ -46,7 +46,9 @@ class cublaslt_resource_factory : public resource_factory {
  */
 inline auto get_cublaslt_handle(resources const& res) -> cublasLtHandle_t
 {
-  res.ensure_default_factory(std::make_shared<cublaslt_resource_factory>());
+  if (!res.has_resource_factory(resource_type::CUBLASLT_HANDLE)) {
+    res.ensure_default_factory(std::make_shared<cublaslt_resource_factory>());
+  }
   return *res.get_resource<cublasLtHandle_t>(resource_type::CUBLASLT_HANDLE);
 };
 
